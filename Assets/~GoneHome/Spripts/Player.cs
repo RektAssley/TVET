6dcb117d85b44b89dfa4157614aed0298f443db8
callum.roscarel2 <callum.roscarel2@600900J211DE406>
2018-04-03 17:57:15 +1000

    extra stuffs

---
+++ Assets/~GoneHome/Spripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		print("hello World lol")
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
