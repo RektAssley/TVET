e2ed8b1736992a315faf7e994fecff631ea9f462
callum.roscarel2 <callum.roscarel2@600900J211DE406>
2018-05-01 17:47:06 +1000

    Script learning

---
+++ Unity/Assets/~GoneHome/Spripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+[] - Brackets
+{} - Braces
+() - Prenthesis
+clean code = CTRL + K + D
+Fold code =  CTRL + M + O
+Unfold code = CTRL + M + P
+*/
+public class Player : MonoBehaviour
+{
+    public string message = "hello world";
+    public Rigidbody rigid;
+
+    float speed = 5;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+            
+    }
+
+}
