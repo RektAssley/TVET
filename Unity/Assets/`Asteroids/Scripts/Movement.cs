03dad3c6961d563b6cde9c08ba0d50368bb31f84
callum.roscarel2 <callum.roscarel2@600900J211DE406>
2018-05-08 17:51:44 +1000

    Asteroids started and Scripted (mostly)

---
+++ Unity/Assets/`Asteroids/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Movement : MonoBehaviour
+    {
+        public float speed = 20f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+
+        void Start()
+        {
+
+            rigid = GetComponent<Rigidbody2D>();
+
+        }
+
+
+        // Update is called once per frame
+
+        void Update()
+        {
+            //Check if a pressed
+            if (Input.GetKey(KeyCode.A))
+            {
+                //Rotate left
+                transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            }
+            //Check if d pressed
+            if (Input.GetKey(KeyCode.D))
+            {
+                //Rotate Right
+                transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+            }
+            //Check if w pressed
+            if (Input.GetKey(KeyCode.W))
+            {
+                //Move in facing direction
+                rigid.AddForce(transform.up * speed);
+            }
+            //Check if s pressed
+            if (Input.GetKey(KeyCode.S))
+            {
+                rigid.AddForce(-transform.up * speed);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/`Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Shooting : MonoBehaviour
+
+    {
+
+        public GameObject bulletPrefab;
+        public float bulletspeed = 20f;
+        public float shootrate = 0.2f;
+
+        private float shootTimer = 0f;
+
+
+        void Shoot()
+        {
+            // Create bullet clone
+            GameObject clone = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            //grab rigidbod from clone
+            Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+            // Add a force to bullet (bulletspeed)
+            rigid.AddForce(transform.up * bulletspeed, ForceMode2D.Impulse);
+        }
+        void Update()
+        {
+            // SET shoottimer = shoottimer + delta time
+            shootTimer += Time.deltaTime;
+            if (shootTimer >= shootrate) ;
+            {
+                //spacebar is down
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //call shot()
+                    Shoot();
+                    shootTimer = 0f;
+                }
+            }
+        }
+
+    }
+}
