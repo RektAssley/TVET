5ab155aa4f725996e13edad2972b73b85f27e8d1
callum.roscarel2 <callum.roscarel2@600900J211DE406>
2018-06-05 18:01:28 +1000

    Non Manny (Learning to UI)

---
+++ Unity/Assets/~NonMannyLesson/Scripts/detect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class detect : MonoBehaviour
+{
+
+    private Collider2D[] col;
+    void Update()
+    {
+
+        if (!Input.GetMouseButton(0))
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        ScreenMouseRay();
+    }
+
+    public void ScreenMouseRay()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5;
+        Vector2 v = Camera.main.ScreenToWorldPoint(mousePosition);
+        col = Physics2D.OverlapPointAll(v);
+        if (col.Length > 0)
+        {
+            foreach (Collider2D c in col)
+            {
+
+                if (c.CompareTag("Button"))
+                {
+                    if(!Input.GetMouseButton(0))
+                    {
+                        c.GetComponent<SpriteRenderer>().color = Color.green;
+                    }
+                    else if(Input.GetMouseButtonDown(0))
+                    {
+                        c.GetComponent<SpriteRenderer>().color = Color.magenta;
+                        Debug.Log("Play");
+                        SceneManager.LoadScene (1);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Unity/Assets/~NonMannyLesson/Scripts/interact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interact : MonoBehaviour {
+
+	//3D Raycasting
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Ray interactRay;
+            //creating a line starting from centre of screen and shooting forward
+            interactRay = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(interactRay, out hitInfo, 10))
+            {
+                if (hitInfo.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Hit Enemy");
+                }
+            }
+        }
+    }
+}
